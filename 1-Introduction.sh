2de45afc3a005963e882cc048f3ad66bf3726375
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-03-05 08:40:44 -0300

    1-Introduction.sh

--- 1-Introduction.sh
+++ 1-Introduction.sh
@@ -5,3 +5,19 @@
         You need to jump in and do it.
 
 '
+
+:'
+    Focus of the book can be summed up in these ways:
+        Beginner to certified professional:
+            As long as you have used a computer, mouse, and keybord, you can star with his book.
+                We tell you how to get Linux, begin using it, step through critical topics, and ultimaely excel at administering and securing it.
+        System administrato-focused:
+            When you are done withthis book, you will not only know how to use Linux, but also modify and maintain it.
+                All the topics needed to become a Red Hat Certified Engineer are covered in this boook.
+        Emphasis on command-line tools:
+            Although point-and-click windows for managing Linux have improved greatly in recent years, 
+            many advanced features can only be utilized by typing commands and editing configuratin files manually.
+            We teach you how to become proficiente with the Linux command-line shell.
+        Aimed at fewer Linux distributions:
+        Many, manydemons an exercises:    
+'
---
+++ 1-Part/1-Chapter/1.1-Starting.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#pag. 45/858
+#file:///L:/Linear_/Base-Jump_03/linux-bible-by-christopher-negus.pdf
