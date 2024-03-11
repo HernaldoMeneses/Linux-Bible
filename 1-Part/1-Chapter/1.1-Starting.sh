866fff1522c35961748e75cc3e5019393ca1e993
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-03-11 13:37:34 -0300

    Update 1.1-Starting.sh

--- 1-Part/1-Chapter/1.1-Starting.sh
+++ 1-Part/1-Chapter/1.1-Starting.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+#One of the most important technology advancements of the twenty-first century.
+
+#Google runs thousands upon thousands of Linux severs to power its seach technology.
+#Its Android phones are based on Linux.
+#Google's Chrome is a browser that is backed by a Linux operating system.
+#Facebook use LAMP (Linux, Apache web server, Mysql database, and PHP)
+
+#Financial organizations tha t have trillions of dollars riding on the speed and security of their operating system also rely heavily on Linux
+## New Yourk Stock Exchange
+## Chicago Mercantile Exchange
+## Tokyo stock Exchange.
+
 
 #pag. 45/858
-#file:///L:/Linear_/Base-Jump_03/linux-bible-by-christopher-negus.pdf
+#file:///L:/Linear_/Linux-Bible/linux-bible-by-christopher-negus.pdf
+#next: understanding What Linux Is
